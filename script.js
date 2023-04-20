19722222db202269c5424a443c1723de8b0edfdc
bamooxa <110097467+Connorw-dev@users.noreply.github.com>
2023-04-20 15:26:37 +1000

    Update template to follow IEEE standards

--- script.js
+++ script.js
@@ -8,14 +8,69 @@ document.getElementById("template-form").addEventListener("submit", (event) => {
   
     const output = `
   ---
-  title: ${title}, ${studentId}, ${subjectCode}
-  author: ${name}
-  date: "\`r Sys.Date()\`"
-  output: pdf_document
+  title: |
+    | \\vspace{8cm} \\LARGE ${title}
+  author: |
+      | ${name}
+      | Student ID: ${studentId}
+  date: \\DTMtoday
+  output:
+    pdf_document:
+      extra_dependencies: float
+      fig_caption: yes
+      number_sections: true
+  header-includes: |
+    \\usepackage{caption}
+    \\usepackage{fancyhdr}
+    \\renewcommand{\\headrulewidth}{0pt}
+    
+    \\usepackage{lastpage}
+    \\usepackage{datetime2}
+    \\usepackage{textcase}
+    \\DTMsetdatestyle{ddmmyyyy}
+    
+    \\pagestyle{fancy}
+    \\fancyhead[L]{${name}}
+    \\fancyhead[C]{${subjectCode} ${title}}
+    \\fancyhead[R]{Date of Submission: \\DTMtoday}
+    \\fancyfoot[C]{}
+    \\fancyfoot[R]{Page \\thepage\\ of \\pageref{LastPage}}
+    
+    
+    \\DeclareCaptionLabelSeparator{IEEE}{.\\quad }
+    \\captionsetup{labelfont=bf}
+    \\captionsetup[figure]{name=Fig., labelsep=IEEE, format=hang, justification=centering,singlelinecheck=false}
+    
+    \\DeclareCaptionTextFormat{up}{\\MakeUppercase{#1}}
+    \\renewcommand{\\thetable}{\\Roman{table}}
+    \\captionsetup[table]{name=TABLE, position=above, justification=centering, labelsep=newline, aboveskip=0pt, textfont=bf, textformat=up}
+    
+    \\usepackage{lscape}
+    \\usepackage{rotating}
   ---
   
+  \\newpage
+  \\tableofcontents
+  \\listoffigures
+  \\listoftables
+  \\newpage
+  
   \`\`\`{r setup, include=FALSE}
-  knitr::opts_chunk$set(echo = TRUE)
+  knitr::opts_chunk$set(echo = FALSE, 
+                        fig.pos = "H",  # Hold figures' position
+                        fig.align = 'center', # Figures go to middle of page
+                        out.extra = "", 
+                        out.width = "75%", # 75% can fit two MATLAB figures per page
+                        message=FALSE,  # Suppress messages when knitting
+                        warning=FALSE,  # Suppress warning when knitting
+                        tidy.opts = list(width.cutoff = 60),  # Wrap code-chunk text
+                        tidy = TRUE)
+  
+  library(ggplot2) # Better plots
+  library(kableExtra) # Better tables
+  library(bookdown) # Mainly for referencing tables, figures, eqns.
+  library(comprehenr)
+  options(scipen=2)  # Prevent scientific notation in plots
   \`\`\`
   `;
   
