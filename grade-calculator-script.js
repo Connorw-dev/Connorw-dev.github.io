e5579152ef0dbf6cb2873d75a809092565eba32e
Connor <110097467+Connorw-dev@users.noreply.github.com>
2023-11-08 11:51:41 +1000

    Add grade calculator math

--- grade-calculator-script.js
+++ grade-calculator-script.js
@@ -1,17 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById('desired-grade').addEventListener('change', handleDesiredGrade);
-  });
-  
-  function searchSubject() {
-    const year = document.getElementById('year').value;
-    const subjectCode = document.getElementById('subject-code').value;
-    const searchUrl = `https://apps.jcu.edu.au/subjectsearch/#/subject/${year}/${subjectCode}`;
-    window.open(searchUrl, '_blank');
-  }
-  
-  function handleDesiredGrade() {
-    const input = document.getElementById('desired-grade').value.toUpperCase();
-    let desiredGrade;
+  document.getElementById('desired-grade').addEventListener('change', handleDesiredGrade);
+});
+
+function searchSubject() {
+  const year = document.getElementById('year').value;
+  const subjectCode = document.getElementById('subject-code').value;
+  const searchUrl = `https://apps.jcu.edu.au/subjectsearch/#/subject/${year}/${subjectCode}`;
+  window.open(searchUrl, '_blank');
+}
+
+function handleDesiredGrade() {
+  const input = document.getElementById('desired-grade').value.toUpperCase();
+  let desiredGrade;
+  if (input.endsWith('%')) {
+    desiredGrade = parseFloat(input);
+  } else {
     switch(input) {
       case 'P': desiredGrade = 50; break;
       case 'C': desiredGrade = 65; break;
@@ -19,33 +22,75 @@ document.addEventListener("DOMContentLoaded", () => {
       case 'HD': desiredGrade = 85; break;
       default:
         desiredGrade = parseFloat(input);
-        if (isNaN(desiredGrade) || desiredGrade < 0 || desiredGrade > 100) {
-          alert('Please enter a valid grade.');
-          return;
-        }
     }
-    console.log(desiredGrade); // Placeholder for desiredGrade handling
   }
-  
-  function addRow() {
-    const table = document.getElementById('grade-table').getElementsByTagName('tbody')[0];
-    const newRow = table.insertRow();
-    const itemCell = newRow.insertCell(0);
-    const weightCell = newRow.insertCell(1);
-    const gradeCell = newRow.insertCell(2);
-  
-    itemCell.innerHTML = '<input type="text" class="item-input">';
-    weightCell.innerHTML = '<input type="number" class="weight-input">';
-    gradeCell.innerHTML = '<input type="number" class="grade-input">';
+  if (isNaN(desiredGrade) || desiredGrade < 0 || desiredGrade > 100) {
+    alert('Please enter a valid grade.');
+    return null;
   }
-  
-  function calculateRest() {
-    const gradeInputs = document.querySelectorAll('.grade-input');
-    gradeInputs.forEach(input => {
-      if (input.value === '') {
-        input.value = '50';
+  return desiredGrade;
+}
+
+function addRow() {
+  const table = document.getElementById('grade-table').getElementsByTagName('tbody')[0];
+  const newRow = table.insertRow();
+  const itemCell = newRow.insertCell(0);
+  const weightCell = newRow.insertCell(1);
+  const gradeCell = newRow.insertCell(2);
+  const removeCell = newRow.insertCell(3);
+
+  itemCell.innerHTML = '<input type="text" class="item-input">';
+  weightCell.innerHTML = '<input type="number" class="weight-input">';
+  gradeCell.innerHTML = '<input type="number" class="grade-input" placeholder="out of 100%">';
+  removeCell.innerHTML = '<button onclick="removeRow(this)">-</button>';
+}
+
+function removeRow(button) {
+  const row = button.parentNode.parentNode;
+  row.parentNode.removeChild(row);
+}
+
+function calculateRest() {
+  const weightInputs = document.querySelectorAll('.weight-input');
+  const gradeInputs = document.querySelectorAll('.grade-input');
+  let totalWeight = 0;
+  let currentGrade = 0;
+  let emptyWeights = 0;
+  let desiredGrade = handleDesiredGrade();
+
+  if (desiredGrade === null) {
+    return; // Stop calculation if desired grade is not valid
+  }
+
+  weightInputs.forEach((weightInput, index) => {
+    const weight = parseFloat(weightInput.value);
+    const grade = parseFloat(gradeInputs[index].value);
+
+    if (!isNaN(weight)) {
+      totalWeight += weight;
+      if (!isNaN(grade)) {
+        currentGrade += (weight / 100) * grade;
+      } else {
+        emptyWeights += weight; // Track the sum of the weights for empty grade fields
+      }
+    }
+  });
+
+  if (totalWeight !== 100) {
+    document.getElementById('error-message').textContent = 'Error: The sum of the weightings must equal 100.';
+    return;
+  }
+
+  document.getElementById('error-message').textContent = ''; // Clear error message if weights are valid
+
+  gradeInputs.forEach((input, index) => {
+    if (input.value === '') {
+      const weight = parseFloat(weightInputs[index].value);
+      if (!isNaN(weight)) {
+        const calculatedGrade = (desiredGrade - (currentGrade / (1 - emptyWeights / 100))) * (100 / emptyWeights);
+        input.value = Math.max(0, calculatedGrade).toFixed(2); // Ensure grades aren't negative
         input.style.color = 'red';
       }
-    });
-  }
-  
+    }
+  });
+}
